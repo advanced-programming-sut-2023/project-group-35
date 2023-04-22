fb169de01d899246133e8232ffa279a133a070fd
alirezamlh <alirezamalekhosseini333@gmail.com>
2023-04-22 19:12:32 +0430

    building menu started

--- src/main/java/view/BuildingMenu.java
+++ src/main/java/view/BuildingMenu.java
@@ -1,8 +1,15 @@
 package view;
 
+import controller.BuildingSelectController;
+
 import java.util.regex.Matcher;
 
 public class BuildingMenu extends Menu{
+    private BuildingSelectController buildingSelectController;
+
+    public BuildingMenu(BuildingSelectController buildingSelectController) {
+        this.buildingSelectController = buildingSelectController;
+    }
 
     public void run() {
 
