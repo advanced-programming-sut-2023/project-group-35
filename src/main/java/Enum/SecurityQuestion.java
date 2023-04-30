2ee988c4f0a9550f1130ec74fdadb60f256453a3
r0naldrailgun <119728749+r0naldrailgun@users.noreply.github.com>
2023-05-01 01:38:23 +0430

    first debugg phase

--- src/main/java/Enum/SecurityQuestion.java
+++ src/main/java/Enum/SecurityQuestion.java
@@ -1,9 +1,9 @@
 package Enum;
 
 public enum SecurityQuestion {
-    FATHER_NAME("what is your father name?"),
-    MOTHER_NAME("what is your mother name?"),
-    PET_NAME("what is your pet name?");
+    FATHER_NAME("1.what is your father name?"),
+    MOTHER_NAME("2.what is your mother name?"),
+    PET_NAME("3.what is your pet name?");
     public String question;
     private SecurityQuestion(String question){
         this.question = question;
