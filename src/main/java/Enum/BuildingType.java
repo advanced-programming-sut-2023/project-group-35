85e758c5feb2112fd8146257894848d0115e897e
alirezamlh <alirezamalekhosseini333@gmail.com>
2023-05-05 20:23:42 +0430

    new field in building types was added

--- src/main/java/Enum/BuildingType.java
+++ src/main/java/Enum/BuildingType.java
@@ -2,49 +2,49 @@ package Enum;
 
 import Enum.*;
 public enum BuildingType {
-    BASE(null, 0 , 0, 0, 0), // مقر
-    SMALL_STONE_GATE( null , 0, 0, 0 , 20),
-    BIG_STONE_GATEHOUSE(Resource.STONE , 20 , 0 , 0 , 20),
-    DRAW_BRIDGE(Resource.WOOD, 10 , 0 , 0 , 20), // پل متحرک
-    LOOKOUT_TOWER(Resource.STONE, 10, 0 , 0 , 20),
-    PERIMETER_TOWER(Resource.STONE, 10 , 0 , 0 , 20),
-    DEFENCE_TURRET(Resource.STONE , 15 , 0 , 0 , 20),
-    SQUARE_TOWER(Resource.STONE, 35 , 0 , 0 , 20),
-    CIRCLE_TOWER(Resource.STONE  , 40 , 0 , 0 , 20),
-    ARMOURY(Resource.WOOD, 5, 0, 0, 20), // TO STORE
-    ARMOURER(Resource.WOOD, 20 , 100 , 1, 20),
+    BASE(null, 0 , 0, 0, 0, 0), // مقر
+    SMALL_STONE_GATE( null , 0, 0, 0 , 20, 0),
+    BIG_STONE_GATEHOUSE(Resource.STONE , 20 , 0 , 0 , 20, 0),
+    DRAW_BRIDGE(Resource.WOOD, 10 , 0 , 0 , 20, 0), // پل متحرک
+    LOOKOUT_TOWER(Resource.STONE, 10, 0 , 0 , 20, 0),
+    PERIMETER_TOWER(Resource.STONE, 10 , 0 , 0 , 20, 0),
+    DEFENCE_TURRET(Resource.STONE , 15 , 0 , 0 , 20, 0),
+    SQUARE_TOWER(Resource.STONE, 35 , 0 , 0 , 20, 0),
+    CIRCLE_TOWER(Resource.STONE  , 40 , 0 , 0 , 20, 0),
+    ARMOURY(Resource.WOOD, 5, 0, 0, 20, 0), // TO STORE
+    ARMOURER(Resource.WOOD, 20 , 100 , 1, 20, 0),
     //WORKSHOP(),
-    BARRACK(Resource.STONE, 15 , 0 , 0 , 20),
-    MERCENARY_CAMP(Resource.WOOD, 10 , 0 , 0 ,0),
-    ENGINEER_GUILD(Resource.WOOD, 10, 100 , 0 , 20),
-    KILLING_PIT(Resource.WOOD , 6 ,0, 0, 20),
-    INN(Resource.WOOD , 20 , 100 , 1 , 0),
-    MILL(Resource.WOOD, 20 , 0 , 3 , 0),
-    IRON_MINE(Resource.WOOD, 20 , 0, 2, 0),
-    SHOP(Resource.WOOD, 5, 0 , 1 , 0),
-    OX_TETHER(Resource.WOOD, 5 , 0, 1 ,0),
-    PITCH_RIG(Resource.WOOD, 20 , 0 , 1 , 0),
-    STONE_MINE(Resource.WOOD, 20 , 0 ,3 , 0),
-    STOCK_PILE(null , 0 , 0 , 0  ,0),
-    WOOD_CUTTER(Resource.WOOD, 3 , 0 , 1 , 0),
-    HOVEL(Resource.WOOD, 6 , 0 , 0 , 20),
-    CHURCH(null , 0 , 250 , 0 , 20),
-    CATHEDRAL(null , 0 , 1000 , 0 , 20),
-    BLACK_SMITH(Resource.WOOD , 20 , 100 , 0 , 20),
-    FLETCHER(Resource.WOOD, 20 , 100 ,0 , 0),
-    POLE_TURNER(Resource.WOOD, 10 , 100, 0 , 0),
-    OIL_SMELTER(Resource.IRON, 100 , 0, 100 , 0),
-    CAGED_WAR_DOGS(Resource.WOOD, 10, 0, 100, 0),
+    BARRACK(Resource.STONE, 15 , 0 , 0 , 20, 0),
+    MERCENARY_CAMP(Resource.WOOD, 10 , 0 , 0 ,0, 0),
+    ENGINEER_GUILD(Resource.WOOD, 10, 100 , 0 , 20, 0),
+    KILLING_PIT(Resource.WOOD , 6 ,0, 0, 20, 0),
+    INN(Resource.WOOD , 20 , 100 , 1 , 0, 0),
+    MILL(Resource.WOOD, 20 , 0 , 3 , 0, 0),
+    IRON_MINE(Resource.WOOD, 20 , 0, 2, 0, 0),
+    SHOP(Resource.WOOD, 5, 0 , 1 , 0, 0),
+    OX_TETHER(Resource.WOOD, 5 , 0, 1 ,0, 0),
+    PITCH_RIG(Resource.WOOD, 20 , 0 , 1 , 0, 0),
+    STONE_MINE(Resource.WOOD, 20 , 0 ,3 , 0, 0),
+    STOCK_PILE(null , 0 , 0 , 0  ,0, 0),
+    WOOD_CUTTER(Resource.WOOD, 3 , 0 , 1 , 0, 0),
+    HOVEL(Resource.WOOD, 6 , 0 , 0 , 20, 0),
+    CHURCH(null , 0 , 250 , 0 , 20, 0),
+    CATHEDRAL(null , 0 , 1000 , 0 , 20, 0),
+    BLACK_SMITH(Resource.WOOD , 20 , 100 , 0 , 20, 0),
+    FLETCHER(Resource.WOOD, 20 , 100 ,0 , 0, 0),
+    POLE_TURNER(Resource.WOOD, 10 , 100, 0 , 0, 0),
+    OIL_SMELTER(Resource.IRON, 100 , 0, 100 , 0, 0),
+    CAGED_WAR_DOGS(Resource.WOOD, 10, 0, 100, 0, 0),
     //SIEGE_TENT(),
-    STABLE(Resource.WOOD, 20, 400, 0 , 0),
-    APPLE_GARDEN(Resource.WOOD, 5, 0, 1, 0),
-    DAIRY_FARM(Resource.WOOD, 10, 0, 1, 0 ),
-    HOP_FARM(Resource.WOOD, 15, 0, 1, 0),
-    HUNTING_GROUND(Resource.WOOD, 5, 0, 1, 0),
-    WHEAT_FARM(Resource.WOOD, 15, 0, 1, 0),
-    BAKERY(Resource.WOOD, 10, 0 , 1, 0),
-    BREWERY(Resource.WOOD, 10 , 0, 1, 0),
-    FOOD_STOCK_PILE(Resource.WOOD, 5, 0, 0, 0);
+    STABLE(Resource.WOOD, 20, 400, 0 , 0, 0),
+    APPLE_GARDEN(Resource.WOOD, 5, 0, 1, 0, 0),
+    DAIRY_FARM(Resource.WOOD, 10, 0, 1, 0 , 0),
+    HOP_FARM(Resource.WOOD, 15, 0, 1, 0, 0),
+    HUNTING_GROUND(Resource.WOOD, 5, 0, 1, 0, 0),
+    WHEAT_FARM(Resource.WOOD, 15, 0, 1, 0, 0),
+    BAKERY(Resource.WOOD, 10, 0 , 1, 0, 0),
+    BREWERY(Resource.WOOD, 10 , 0, 1, 0, 0),
+    FOOD_STOCK_PILE(Resource.WOOD, 5, 0, 0, 0, 0);
 
 //    TUNNEL(),
 //
@@ -64,14 +64,16 @@ public enum BuildingType {
     public final int workersNumber;
 
     public final int hp;
+    private final int rateOrCapacity;
 
-    BuildingType(Resource resourceToBuild, int resourceAmount, int goldCost, int workersNumber, int hp) {
+    BuildingType(Resource resourceToBuild, int resourceAmount, int goldCost, int workersNumber, int hp, int rateOrCapacity) {
         //Group = group;
         this.resourceToBuild = resourceToBuild;
         this.resourceAmount = resourceAmount;
         this.goldCost = goldCost;
         this.workersNumber = workersNumber;
         this.hp = hp;
+        this.rateOrCapacity = rateOrCapacity;
     }
 
     public String getName() {
@@ -93,6 +95,10 @@ public enum BuildingType {
             if(type.equals(this)) return true;
         }
         return false;
+    }
+
+    public int getRelatedInteger() {
+        return this.rateOrCapacity;
     }
 
 }
